f44081ea187c47bc276fdfdbdb877a2e27bbae13
zyymax <zyy.max@gmail.com>
2014-06-17 12:44:50 +0800

    delete file

--- src/web/web.py
+++ src/web/web.py
@@ -11,8 +11,6 @@ import tornado.web
 from tornado.options import define, options
 
 from handlers.home import HomeHandler
-from handlers.more import MoreHandler
-from handlers.about import AboutHandler
 from handlers.search import SearchLucHandler
 
 define("port", default=8080, help="run on the given port", type=int)
@@ -20,12 +18,11 @@ define("port", default=8080, help="run on the given port", type=int)
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", HomeHandler),
-            (r"/more/([a-z0-9A-Z\_\-]+)/([a-z]+)", MoreHandler),
-            (r"/about", AboutHandler),
             (r"/search_luc", SearchLucHandler),
             ]
         settings = dict(
@@ -38,14 +35,16 @@ class Application(tornado.web.Application):
             )
         tornado.web.Application.__init__(self, handlers, **settings)
 
+
 class WebApp(object):
     def __init__(self):
         pass
+
     def run(self):
         tornado.options.parse_command_line()
         http_server = tornado.httpserver.HTTPServer(Application(), xheaders=True)
         http_server.listen(options.port)
-        tornado.ioloop.IOLoop.instance().start()        
+        tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
     WebApp().run()
